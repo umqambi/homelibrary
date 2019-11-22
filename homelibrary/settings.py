2a6b2e1a2ae7f08c6786b7aa37407deba5cc07c8
umqambi <umqambi@tuta.io>
2019-11-22 12:48:26 +0300

    change settings for deployment to heroku

--- homelibrary/settings.py
+++ homelibrary/settings.py
@@ -126,3 +126,12 @@ STATIC_URL = '/static/'
 # Media path
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
